6be4a00640d32bb60b993bd2a27497dab4d658fe
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-02 14:27:37 -0400

    Planned Features and Started Writing Code

---
+++ src/Grapher.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Stefan on 8/2/2015.
+ */
+public class Grapher {
+
+    /**
+     * The height, in pixels, of the graph to be generated.
+     * A minimum value of
+     */
+    private static final int HEIGHT = 400;
+
+    /**
+     * The width, in pixels, of the graph to be generated.
+     * A minimum value of
+     */
+    private static final int WIDTH = 400;
+
+    private static final long Y_MIN = -10;
+    private static final long Y_MAX = 10;
+    private static final long X_MIN = -10;
+    private static final long X_MAX = 10;
+
+    // todo: check that range makes sense; Add documentation
+
+    
+}
